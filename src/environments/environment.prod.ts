8f77aed20119e5d768bb80512cb13e541cd35947
nitinthomas20 <nitinthomas20@gmail.com>
2026-02-20 19:13:39 +1100

    chore: add environment configuration files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://carhub-417485167247.europe-west1.run.app/api',
+   //apiBaseUrl: 'http://localhost:3000/api',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  //apiBaseUrl: 'https://firehawk-417485167247.europe-west1.run.app/api',
+  apiBaseUrl: 'https://carhub-417485167247.europe-west1.run.app/api',
+};
